860c08f1519df29e9bca9a4eeddefa6d446ad014
Sean Gallagher <stgallag@gmail.com>
2021-04-16 22:00:03 -0400

    Convention over configuration

--- src/dbopt.rs
+++ src/dbopt.rs
@@ -11,16 +11,12 @@ pub type PooledSqlite = PooledConnection<ConnectionManager<SqliteConnection>>;
 
 #[derive(StructOpt)]
 #[structopt(rename_all = "kebab-case")]
-pub struct DbOpt {
-    /// How to connect to the postgres database.
-    #[structopt(long, env = "DATABASE_URL", hide_env_values = true)]
-    db_url: String,
-}
+pub struct DbOpt {}
 
 impl DbOpt {
     pub fn connect(&self) -> Result<SqliteConnection, ConnectionError> {
         let time = Instant::now();
-        let db = SqliteConnection::establish(&self.db_url)?;
+        let db = SqliteConnection::establish("rphotos.db")?;
         debug!("Got db connection in {:?}", time.elapsed());
         Ok(db)
     }
@@ -30,7 +26,7 @@ impl DbOpt {
             .min_idle(Some(2))
             .test_on_check_out(false)
             .connection_timeout(Duration::from_millis(500))
-            .build(ConnectionManager::new(&self.db_url))?;
+            .build(ConnectionManager::new("rphotos.db"))?;
         debug!("Created pool in {:?}", time.elapsed());
         Ok(pool)
     }
